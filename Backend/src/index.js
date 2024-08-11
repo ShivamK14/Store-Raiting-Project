f3042aaa087b8c213df93855ea67b10b2018f495
ShivamK14 <shivam.karle@gmail.com>
2024-08-11 21:39:06 +0530

    new changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,12 +5,33 @@ import userRouter from "./routes/userRoutes.js";
 import storeRouter from "./routes/storeRouter.js";
 import path from "path";
 import mongoose from "mongoose";
+import cors from "cors";
+import { callbackify } from "util";
+const production = [process.env.Origin_1];
+const devOrigin = ["http://localhost:5173"];
+const allowedorigin =
+  process.env.NODE_ENV === "production" ? production : devOrigin;
+
 const secret_key = process.env.SECRET_KEY;
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log(origin, allowedorigin);
+      if (allowedorigin.includes(origin)) {
+        console.log(origin, allowedorigin);
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by cors"));
+      }
+    },
+    credentials: true,
+    methods: ["PUT", "DELETE"],
+  })
+);
 const __dirname = path.resolve();
 app.use((req, res, next) => {
   console.log("HTTP Method - " + req.method + " - URL " + req.url);
