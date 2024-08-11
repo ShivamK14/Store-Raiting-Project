c70c7e52efb0205e0db61d4af275a4854066d7f9
ShivamK14 <shivam.karle@gmail.com>
2024-08-11 11:45:41 +0530

    make changes

--- Frontend/AppFrontend/vite.config.js
+++ Frontend/AppFrontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Store-Raiting-Project",
   server: {
     proxy: {
       "/api": "http://localhost:5000/",
